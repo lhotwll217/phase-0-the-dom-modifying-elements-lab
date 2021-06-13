1bb99f18f79dd701c667585bf6ddd582020d825b
Luke Otwell <lukehenryotwell@gmail.com>
2021-06-13 19:45:42 -0400

    Finished Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+main = document.querySelector('main');
+
+main.remove();
+
+//const body = document.querySelector('body');
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Luke is the champion';
+
+
